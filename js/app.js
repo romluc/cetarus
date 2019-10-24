ed4186dfddda734bac22172d1ab7b5a24bf3814d
romluc <romulofilhofilho@gmail.com>
2019-10-24 19:30:46 -0300

    Delete signed.html

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: 'AIzaSyCIA1Ye7SPqXsmaGZdZPfm1ecJu7IqUagY',
+	authDomain: 'cetarus-baas.firebaseapp.com',
+	databaseURL: 'https://cetarus-baas.firebaseio.com',
+	projectId: 'cetarus-baas',
+	storageBucket: 'cetarus-baas.appspot.com',
+	messagingSenderId: '73670084882',
+	appId: '1:73670084882:web:0c04c3bb5deeddefe9cbcb',
+	measurementId: 'G-691RCRTQTD'
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
